6430c138c7ab0105bbba12440908ee7dc1931fdc
Newstein123 <102919096+Newstein123@users.noreply.github.com>
2024-02-11 23:49:15 +0630

    first commit

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,7 +48,7 @@ return [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
+        'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
@@ -71,7 +71,6 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
-        public_path('storage') => storage_path('app/resources'),
     ],
 
 ];
